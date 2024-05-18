d1501ac6da44f2857e244720621761d117ba515d
root <root@ip-172-31-15-211.ap-south-1.compute.internal>
2024-05-18 05:45:43 +0000

    .env added

--- index.js
+++ index.js
@@ -16,7 +16,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-
+require('dotenv').config();
 const { Codecs } = require("./lib/codecs");
 const { Languages } = require("./lib/languages");
 
